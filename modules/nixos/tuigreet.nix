e72f6d76410b0114d978cb28ae93fbba25f6ad8e
Osi Bluber <87434959+OsiPog@users.noreply.github.com>
2025-07-18 14:17:52 +0200

    Successful Rebuild

--- modules/nixos/tuigreet.nix
+++ modules/nixos/tuigreet.nix
@@ -3,13 +3,16 @@
   config,
   ...
 }: {
+  environment.systemPackages = [ pkgs.xorg.xauth ];
+  services.xserver.displayManager.startx.enable = true;
+
   services.greetd = {
     enable = true;
     settings = {
       # user needs to authenticate on relogin
       default_session = {
         # https://ryjelsum.me/homelab/greetd-session-choose/
-        command = ''${pkgs.greetd.tuigreet}/bin/tuigreet --sessions ${config.services.displayManager.sessionData.desktops}/share/xsessions:${config.services.displayManager.sessionData.desktops}/share/wayland-sessions --remember --remember-user-session'';
+        command = ''${pkgs.greetd.tuigreet}/bin/tuigreet --sessions ${config.services.displayManager.sessionData.desktops}/share/xsessions:${config.services.displayManager.sessionData.desktops}/share/wayland-sessions --xsession-wrapper startx --remember --remember-user-session'';
         user = "greeter";
       };
     };
