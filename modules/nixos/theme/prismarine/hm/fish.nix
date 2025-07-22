ad59337c5dad64ee6a64e7d60ebd865ac14efe0f
Osi Bluber <87434959+OsiPog@users.noreply.github.com>
2025-07-22 18:44:50 +0200

    Successful Rebuild

--- modules/nixos/theme/prismarine/hm/fish.nix
+++ modules/nixos/theme/prismarine/hm/fish.nix
@@ -12,7 +12,7 @@
         owner = "oh-my-fish";
         repo = "theme-bobthefish";
         rev = "e3b4d4eafc23516e35f162686f08a42edf844e40";
-        sha256 = "sha256-OBku4wwMROu3HQXkaM26qhL0SIEtz8ShypuLcpbxp78=";
+        sha256 = "sha256-cXOYvdn74H4rkMWSC7G6bT4wa9d3/3vRnKed2ixRnuA=";
       };
     }
   ];
