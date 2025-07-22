5abd11acf6127bc681cfea797d58a0f2f3ecf6c9
Osi Bluber <87434959+OsiPog@users.noreply.github.com>
2025-07-22 18:49:11 +0200

    Successful Rebuild

--- modules/nixos/theme/prismarine/hm/fish.nix
+++ modules/nixos/theme/prismarine/hm/fish.nix
@@ -7,10 +7,10 @@
   programs.fish.plugins = lib.mkIf config.programs.fish.enable [
     # A nice theme
     {
-      name = "bobthefish";
+      name = "theme";
       src = pkgs.fetchFromGitHub {
         owner = "oh-my-fish";
-        repo = "theme-bobthefish";
+        repo = "theme-cbjohnson";
         rev = "e3b4d4eafc23516e35f162686f08a42edf844e40";
         sha256 = "sha256-cXOYvdn74H4rkMWSC7G6bT4wa9d3/3vRnKed2ixRnuA=";
       };
