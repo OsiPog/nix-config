aac4049a06ce34cf05fc3c9d32a00a2553dcecc6
Osi Bluber <87434959+OsiPog@users.noreply.github.com>
2025-08-22 09:54:45 +0200

    Successful Rebuild

--- modules/nixos/theme/prismarine/hm/hyprland.nix
+++ modules/nixos/theme/prismarine/hm/hyprland.nix
@@ -58,7 +58,7 @@ in {
       ];
       windowrule = [
         "pin, class:(w|r)ofi"
-        "size 1% 1%, class:(w|r)ofi"
+        "size 100 100, class:(w|r)ofi"
         "center, class:(w|r)ofi"
         # "stayfocused, class:(w|r)ofi" # cant click outside
         # "dimaround, class:(w|r)ofi"
