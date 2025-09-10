4a2d364e5935204c8aa073ab79b9c08eb13650f0
Osi Bluber <87434959+OsiPog@users.noreply.github.com>
2025-09-10 17:08:59 +0200

    fix(shared): remove unused file and buffer size

--- modules/nixos/shared/nix-settings.nix
+++ modules/nixos/shared/nix-settings.nix
@@ -16,6 +16,8 @@
     extraOptions = ''
       extra-substituters = https://devenv.cachix.org
       extra-trusted-public-keys = devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=
+
+      download-buffer-size = 134217728
     '';
   };
 
