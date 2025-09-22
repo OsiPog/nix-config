6e3b3ddd6295b3d10afd6edbec92ac987546d17c
Osi Bluber <87434959+OsiPog@users.noreply.github.com>
2025-09-22 21:53:57 +0200

    Successful Rebuild

--- users/osi/home.nix
+++ users/osi/home.nix
@@ -3,57 +3,55 @@
   lib,
   inputs,
   ...
-}:
-{
+}: {
   home = rec {
     username = "osi";
     homeDirectory = "/home/${username}";
     stateVersion = lib.mkDefault "25.11";
   };
 
-  imports =
-    with flake.homeModules;
-    with inputs.nix-config-private.homeModules;
-    [
-      # shell
-      fish
-      git
+  imports = with flake.homeModules;
+  with inputs.nix-config-private.homeModules; [
+    # shell
+    fish
+    git
 
-      # terminal
-      kitty
+    # terminal
+    kitty
 
-      # Window manager
-      hyprland # base config
-      hypr-laptop # for laptops
-      hypr-touch # for laptops
-      waybar # utility bar
-      hypr-lockscreen # lockscreen with auto enable on inactivity
-      hypr-runner
-      hypr-workspaces
+    # Window manager
+    hyprland # base config
+    hypr-laptop # for laptops
+    hypr-touch # for laptops
+    waybar # utility bar
+    hypr-lockscreen # lockscreen with auto enable on inactivity
+    hypr-runner
+    hypr-workspaces
+    quickshell
 
-      # code editor
-      vscode
-      # nvf
-      helix
+    # code editor
+    vscode
+    # nvf
+    helix
 
-      # web browser
-      firefox
-      chromium # when firefox fails
+    # web browser
+    firefox
+    chromium # when firefox fails
 
-      # password manager
-      password-store
+    # password manager
+    password-store
 
-      # Some common desktop apps I need
-      desktop-apps
-      cli-tools
+    # Some common desktop apps I need
+    desktop-apps
+    cli-tools
 
-      # syncing files
-      syncthing
+    # syncing files
+    syncthing
 
-      # private
-      uni
-      work
-    ];
+    # private
+    uni
+    work
+  ];
 
   programs.gpg.publicKeys = [
     {
