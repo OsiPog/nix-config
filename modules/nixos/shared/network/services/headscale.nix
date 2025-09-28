ffe7edc1f0e24922510488d14822257e033f7a29
Osi Bluber <87434959+OsiPog@users.noreply.github.com>
2025-09-28 20:25:40 +0200

    Successful Rebuild

--- modules/nixos/shared/network/services/headscale.nix
+++ modules/nixos/shared/network/services/headscale.nix
@@ -19,6 +19,7 @@ in {
       address = "0.0.0.0";
       port = cfg.port;
       settings = {
+        prefixes.v4 = "10.0.0.0/24";
         server_url = "https://" + (config.lib.network.toFullDomain "headscale");
         dns = {
           override_local_dns = true;
