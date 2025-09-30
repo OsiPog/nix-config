98983220d8cd5b68676587bcd016b908cf15e945
Osi Bluber <87434959+OsiPog@users.noreply.github.com>
2025-09-30 16:33:50 +0200

    Successful Rebuild

--- hosts/haunt-muskie/configuration.nix
+++ hosts/haunt-muskie/configuration.nix
@@ -17,87 +17,15 @@
     helix
   ];
 
-  services.phpfpm.pools.laravel = {
-    user = "laravel";
-    settings = {
-      "listen.owner" = config.services.nginx.user;
-      "pm" = "dynamic";
-      "pm.max_children" = 32;
-      "pm.max_requests" = 500;
-      "pm.start_servers" = 2;
-      "pm.min_spare_servers" = 2;
-      "pm.max_spare_servers" = 5;
-      "php_admin_value[error_log]" = "stderr";
-      "php_admin_flag[log_errors]" = true;
-      "catch_workers_output" = true;
-    };
-    phpPackage = pkgs.php.buildEnv {
-      extensions = {
-        enabled,
-        all,
-      }:
-        enabled
-        ++ (with all; [
-          xdebug
-          ctype
-          curl
-          dom
-          fileinfo
-          filter
-          mbstring
-          openssl
-          pdo
-          session
-          tokenizer
-        ]);
-      extraConfig = ''
-        xdebug.mode=debug
-      '';
-    };
-  };
-  services.mysql = {
-    package = pkgs.mariadb;
-    enable = true;
-  };
+  networking.firewall.allowedTCPPorts = [5173];
 
   services.nginx.virtualHosts."laravel-application.${config.networking.domain}" = {
     useACMEHost = "${config.networking.domain}-cert";
     forceSSL = true;
-    root = "/srv/laravel/public";
     locations = {
-      "/".tryFiles = "$uri $uri/ /index.php?$query_string";
-      "= /favicon.ico".extraConfig = ''
-        access_log off;
-        log_not_found off;
-      '';
-      "= /robots.txt".extraConfig = ''
-        access_log off;
-        log_not_found off;
-      '';
-      "~ ^/index\.php(/|$)".extraConfig = ''
-        fastcgi_pass unix:${config.services.phpfpm.pools.laravel.socket};
-        include ${pkgs.nginx}/conf/fastcgi.conf;
-        fastcgi_hide_header X-Powered-By;
-        fastcgi_split_path_info ^(.+\.php)(/.+)$;
-      '';
+      "/".proxyPass = "http://localhost:8000";
     };
-    extraConfig = ''
-      add_header X-Frame-Options "SAMEORIGIN";
-      add_header X-Content-Type-Options "nosniff";
-
-      index index.php;
-
-      charset utf-8;
-
-      error_page 404 /index.php;
-    '';
   };
-
-  users.users.laravel = {
-    isSystemUser = true;
-    group = "laravel";
-  };
-  users.groups.laravel = {};
 
   security.acme.certs."${config.networking.domain}-cert".extraDomainNames = ["laravel-application.${config.networking.domain}"];
 
