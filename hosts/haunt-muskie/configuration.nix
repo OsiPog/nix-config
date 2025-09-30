f92a8bb25ed0d10f7d20185868ffbf6ff1fa27c1
Osi Bluber <87434959+OsiPog@users.noreply.github.com>
2025-09-30 15:21:01 +0200

    Successful Rebuild

--- hosts/haunt-muskie/configuration.nix
+++ hosts/haunt-muskie/configuration.nix
@@ -15,19 +15,63 @@
   environment.systemPackages = with pkgs; [
     devenv
     helix
+    (pkgs.php.buildEnv {
+      extensions = {
+        enabled,
+        all,
+      }:
+        enabled
+        ++ (with all; [
+          xdebug
+          ctype
+          curl
+          dom
+          fileinfo
+          filter
+          mbstring
+          openssl
+          pdo
+          session
+          tokenizer
+        ]);
+      extraConfig = ''
+        xdebug.mode=debug
+      '';
+    })
   ];
 
   services.nginx.virtualHosts."laravel-application.${config.networking.domain}" = {
     useACMEHost = "${config.networking.domain}-cert";
     forceSSL = true;
+    root = "/srv/laravel/public";
     locations = {
-      "/" = {
-        proxyPass = "http://localhost:8000";
-      };
-      "/@vite" = {
-        proxyPass = "http://localhost:5173";
-      };
+      "/".tryFiles = "$uri $uri/ /index.php?$query_string";
+      "= /favicon.ico".extraConfig = ''
+        access_log off;
+        log_not_found off;
+      '';
+      "= /robots.txt".extraConfig = ''
+        access_log off;
+        log_not_found off;
+      '';
+      "~ ^/index\.php(/|$)".extraConfig = ''
+        fastcgi_pass unix:/var/run/php/php8.2-fpm.sock;
+        fastcgi_param SCRIPT_FILENAME $realpath_root$fastcgi_script_name;
+        include fastcgi_params;
+        fastcgi_hide_header X-Powered-By;
+      '';
+      "~ /\.(?!well-known).*".extraConfig = "deny all;";
     };
+    extraConfig = ''
+      add_header X-Frame-Options "SAMEORIGIN";
+      add_header X-Content-Type-Options "nosniff";
+
+      index index.php;
+
+      charset utf-8;
+
+      error_page 404 /index.php;
+    '';
   };
 
   security.acme.certs."${config.networking.domain}-cert".extraDomainNames = ["laravel-application.${config.networking.domain}"];
