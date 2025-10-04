b400fc0df1b01c6cebd321185111c51287c34b4e
Osi Bluber <87434959+OsiPog@users.noreply.github.com>
2025-10-04 14:36:57 +0200

    Successful Rebuild

--- modules/nixos/shared/network/services/authelia.nix
+++ modules/nixos/shared/network/services/authelia.nix
@@ -14,13 +14,19 @@ in {
     (mkServiceOptionsModule "authelia")
   ];
   config = mkIf (cfg.enable && cfg.host == hostName) {
+    sops.secrets = {
+      "authelia/jwtSecret" = {owner = config.services.authelia.instances.default.user;};
+      "authelia/storageEncryptionKey" = {owner = config.services.authelia.instances.default.user;};
+    };
+
     services.authelia.instances.default = {
       enable = true;
+      secrets = {
+        jwtSecretFile = config.getSopsFile "authelia/jwtSecret";
+        storageEncryptionKeyFile = config.getSopsFile "authelia/storageEncryptionKey";
+      };
       settings = {
-        server = {
-          host = "0.0.0.0";
-          port = cfg.port;
-        };
+        address = "http://0.0.0.0:${toString cfg.port}";
         log = {
           level = "info";
         };
