47c63ae67526e7b9f575f291f65d4dfaed13184f
Osi Bluber <87434959+OsiPog@users.noreply.github.com>
2025-10-14 23:25:40 +0200

    mailserver

---
+++ modules/nixos/shared/network/services/mailserver/default.nix
@@ -0,0 +1,48 @@
+# Some extra steps are needed after install more here: https://nixos-mailserver.readthedocs.io/en/latest/setup-guide.html
+{
+  inputs,
+  config,
+  hostName,
+  lib,
+  flake,
+  ...
+}: let
+  inherit (lib) mkIf;
+  inherit (flake.lib) mkServiceOptionsModule;
+
+  cfg = config.network.services.mailserver;
+in {
+  imports = [
+    inputs.simple-nixos-mailserver.nixosModules.default
+    (mkServiceOptionsModule "mailserver")
+  ];
+
+  config = mkIf (cfg.enable && cfg.host == hostName) {
+    assertions = [
+      {
+        assertion = !cfg.reverseProxy.enable;
+        message = "Due to mail protocol requirements, mailserver cannot be reverse proxied.";
+      }
+    ];
+
+    sops.secrets."mailserver/pass-hashes/admin" = {sopsFile = ./secrets.yaml;};
+
+    mailserver = {
+      enable = true;
+      stateVersion = 3;
+      fqdn = "mail.${config.networking.domain}";
+      domains = [config.networking.domain];
+
+      # A list of all login accounts. To create the password hashes, use
+      # nix-shell -p mkpasswd --run 'mkpasswd -sm bcrypt'
+      loginAccounts = {
+        "admin@${config.networking.domain}" = {
+          hashedPasswordFile = config.getSopsFile "mailserver/pass-hashes/admin";
+        };
+      };
+
+      certificateScheme = "acme";
+      acmeCertificateName = "default";
+    };
+  };
+}
