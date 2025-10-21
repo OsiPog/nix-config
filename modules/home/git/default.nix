6c8e56a343c4253bd2269a41664dac70ca54fa78
Osi Bluber <87434959+OsiPog@users.noreply.github.com>
2025-10-21 19:23:56 +0200

    feat(git): new `git-smart-clone` command

--- modules/home/git/default.nix
+++ modules/home/git/default.nix
@@ -1,70 +1,26 @@
 {
   pkgs,
-  lib,
   config,
   ...
-}: let
-  inherit (builtins) listToAttrs concatStringsSep;
-  inherit (lib) pipe flatten concatLines;
-
-  authorKeys = [
-    "primary"
-    "secondary"
-  ];
-in {
+}: {
   home.packages = with pkgs; [
     lazygit
-    (writeShellApplication {
-      name = "git-set-author";
-      runtimeInputs = with pkgs; [
-        git
-        fzf
-        jq
-      ];
-      text = ''
-        declare -A names
-        ${pipe authorKeys [
-          (map (author: ''names["${author}"]=$(cat ${config.getSopsFile "git-authors/${author}/name"})''))
-          concatLines
-        ]}
-        declare -A emails
-        ${pipe authorKeys [
-          (map (author: ''emails["${author}"]=$(cat ${config.getSopsFile "git-authors/${author}/email"})''))
-          concatLines
-        ]}
-
-        author=$(echo -e "${
-          pipe authorKeys [
-            (map (author: "${author} - \${names['${author}']}"))
-            (concatStringsSep "\\n")
-          ]
-        }" | fzf | awk '{print $1}')
-
-        git config user.name "''${names["$author"]}"
-        git config user.email "''${emails["$author"]}"
-      '';
-    })
   ];
 
-  sops.secrets =
-    {
-      "ssh-keys/gh-primary/private" = {sopsFile = ./secrets.yaml;};
-      "ssh-keys/gh-secondary/private" = {sopsFile = ./secrets.yaml;};
-    }
-    // (pipe authorKeys [
-      (map (author: [
-        {
-          name = "git-authors/${author}/name";
-          value = {sopsFile = ./secrets.yaml;};
-        }
-        {
-          name = "git-authors/${author}/email";
-          value = {sopsFile = ./secrets.yaml;};
-        }
-      ]))
-      flatten
-      listToAttrs
-    ]);
+  programs.fish.functions.git-smart-clone =
+    /*
+    fish
+    */
+    ''
+      nu ${./git-smart-clone.nu} --authors-json "$(cat ${config.getSopsFile "git-authors-json"})" $argv
+      cd $(cat /tmp/git-smart-clone-cd)
+    '';
+
+  sops.secrets = {
+    "ssh-keys/gh-primary/private" = {sopsFile = ./secrets.yaml;};
+    "ssh-keys/gh-secondary/private" = {sopsFile = ./secrets.yaml;};
+    "git-authors-json" = {sopsFile = ./secrets.yaml;};
+  };
 
   programs.git = {
     enable = true;
---
+++ modules/home/git/git-smart-clone.nu
@@ -0,0 +1,33 @@
+def --wrapped main [url: string --base-dir: path = "~/repositories" --authors-json: string ...rest] {
+  let $authors = if ($authors_json != null) {
+      $authors_json | from json
+    } else {
+      []
+    }
+    | each {insert "display" $"($in.name) - ($in.email)"}
+
+  let directory = $url
+    | str downcase
+    | str replace "https://" ""
+    | str replace "http://" ""
+    # remove sshuser@ prefix
+    | split row "@" | last | str join ""
+    | str replace ":" "/"
+    | do {($base_dir | str replace "~" $env.HOME) + "/" + $in}
+
+  mkdir $directory
+
+  git clone $url $directory ...$rest
+
+  zoxide add $directory
+
+  cd $directory
+  $directory | save --force /tmp/git-smart-clone-cd # hack that we can cd into the new directory after calling `nu git-smart-clone.nu`
+
+  # Ask the user which author
+  if (($authors | length) > 0) {
+    let author = $authors | input list "Select Author" --fuzzy --display display
+    git config user.name $author.name
+    git config user.email $author.email        
+  }
+}
