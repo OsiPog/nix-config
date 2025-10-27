a07a0c0187dea4f3f9c4148333fe9d9724df140d
Osi Bluber <87434959+OsiPog@users.noreply.github.com>
2025-10-27 22:22:16 +0100

    fix(flake): syntax of nixosHostNames function

--- lib/flake/nixosHostNames.nix
+++ lib/flake/nixosHostNames.nix
@@ -1,11 +1,7 @@
 # Returns a list of all host names (directory names in hosts/) that contain a configuration.nix file
-{
-  flake,
-  inputs,
-  ...
-}: let
+flake: let
   inherit (builtins) attrNames readDir pathExists filter;
-  inherit (inputs.nixpkgs.lib) pipe;
+  inherit (flake.inputs.nixpkgs.lib) pipe;
 
   hostsPath = "${flake}/hosts";
 in
