8c49ed75e85299e46ce91fd91e944609c944d682
Osi Bluber <87434959+OsiPog@users.noreply.github.com>
2025-10-30 20:18:38 +0100

    feat(uni): add ag-git ssh connection

--- modules/home/uni/default.nix
+++ modules/home/uni/default.nix
@@ -6,5 +6,6 @@
   sops.secrets = {
     "ssh-keys/uni-gitlab/private" = {sopsFile = ./secrets.yaml;};
     "ssh-keys/ag-vps/private" = {sopsFile = ./secrets.yaml;};
+    "ssh-keys/ag-git/private" = {sopsFile = ./secrets.yaml;};
   };
 }
