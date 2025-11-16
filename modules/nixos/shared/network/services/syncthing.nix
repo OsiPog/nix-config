02c7a2d30b6e49b042d65339ee2135af0fe0eec7
Osi Bluber <87434959+OsiPog@users.noreply.github.com>
2025-11-16 19:58:36 +0100

    fix(syncthing): correctly convert int to string

--- modules/nixos/shared/network/services/syncthing.nix
+++ modules/nixos/shared/network/services/syncthing.nix
@@ -54,7 +54,7 @@ in {
     services.syncthing = {
       enable = true;
       openDefaultPorts = true;
-      guiAddress = "127.0.0.1:${cfg.ports.web.port}";
+      guiAddress = "127.0.0.1:${toString cfg.ports.web.port}";
       settings = {
         devices = pipe networkCfg.hosts [
           (filterAttrs (name: host: host.services.${serviceName}.enable && name != hostName))
