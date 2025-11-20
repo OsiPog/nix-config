13155b502db4a5a55529537bc37f7ddc2d1aba04
Osi Bluber <87434959+OsiPog@users.noreply.github.com>
2025-11-20 14:10:53 +0100

    chore(minecraft-server): add comment about geyser versioning

--- modules/nixos/shared/network/services/minecraft-server.nix
+++ modules/nixos/shared/network/services/minecraft-server.nix
@@ -52,9 +52,12 @@ in {
         package = pkgs.minecraftServers.paper-1_21_10;
         symlinks = {
           # --- Plugins
+          # Regarding Geyser download.geysermc.org downloads, version pinning is not perfect but its fine. (url still points to latest build)
+          # To get all versions of a project query: https://download.geysermc.org/v2/projects/$PROJECT/versions
+          #
           # Geyser, allow Bedrock players to connect to java server
           "plugins/Geyser-Spigot.jar" = fetchurl {
-            url = "https://download.geysermc.org/v2/projects/geyser/versions/latest/builds/latest/downloads/spigot";
+            url = "https://download.geysermc.org/v2/projects/geyser/versions/2.9.0/builds/latest/downloads/spigot";
             hash = "sha256-rJcKRpLzkwEXZuHg/JiOfFVNLWJNy1j11sgZc4I4UcA=";
           };
           # "plugins/Geyser-Spigot/config.yml".value = {
@@ -62,7 +65,7 @@ in {
           # };
           # better compatibility for bedrock players
           "plugins/floodgate-spigot.jar" = fetchurl {
-            url = "https://download.geysermc.org/v2/projects/floodgate/versions/latest/builds/latest/downloads/spigot";
+            url = "https://download.geysermc.org/v2/projects/floodgate/versions/2.2.5/builds/latest/downloads/spigot";
             hash = "sha256-c5N3B5s2L67M9dFvMUK6A0CphupISzcPIOymngWBDrY=";
           };
         };
