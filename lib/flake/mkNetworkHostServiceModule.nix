b8f725d6f9dcafcdb7b5daacc234372353421d7e
Osi Bluber <87434959+OsiPog@users.noreply.github.com>
2025-12-29 13:08:19 +0100

    fix: correct functions in helper

--- lib/flake/mkNetworkHostServiceModule.nix
+++ lib/flake/mkNetworkHostServiceModule.nix
@@ -2,16 +2,16 @@ flake: {
   serviceName,
   withEnable ? true,
 }: module: {lib, ...}: let
-  inherit (lib) mkEnableOption optionals;
+  inherit (lib) mkEnableOption optional;
 in {
   network.sharedModules = [
     (moduleArgs: {
       imports =
         []
-        ++ (optionals (module != null) [
-          (module (moduleArgs // {cfg = moduleArgs.config.services.${serviceName};}))
-        ])
-        ++ (optionals withEnable {
+        ++ (optional (module != null) (
+          module (moduleArgs // {cfg = moduleArgs.config.services.${serviceName};})
+        ))
+        ++ (optional withEnable {
           options.services.${serviceName}.enable = mkEnableOption "the ${serviceName} network service on ${moduleArgs.name}";
         });
     })
