4b3734f1a50e773f2bf031186c400ff449328dc4
Osi Bluber <87434959+OsiPog@users.noreply.github.com>
2026-01-18 16:00:32 +0100

    fix(floating-trees): disable beep unlock

--- hosts/floating-trees/configuration.nix
+++ hosts/floating-trees/configuration.nix
@@ -16,32 +16,32 @@
 
   disko.devices.disk.disk1.device = "/dev/disk/by-id/ata-EDILOCA_ES106_1TB_AA000000000000050186";
 
-  systemd.services.beep-unlocked = {
-    path = [pkgs.beep];
-    wantedBy = ["basic.target"];
-    script = ''
-      G5=784
-      F5S=740
-      D5S=622
-      A4=440
-      G4S=415
-      E5=659
-      G5S=831
-      C6=1047
+  # systemd.services.beep-unlocked = {
+  #   path = [pkgs.beep];
+  #   wantedBy = ["basic.target"];
+  #   script = ''
+  #     G5=784
+  #     F5S=740
+  #     D5S=622
+  #     A4=440
+  #     G4S=415
+  #     E5=659
+  #     G5S=831
+  #     C6=1047
 
-      SPEED=120
+  #     SPEED=120
 
-      beep -f $G5 -l $SPEED
-      beep -f $F5S -l $SPEED
-      beep -f $D5S -l $SPEED
-      beep -f $A4 -l $SPEED
-      beep -f $G4S -l $SPEED
-      beep -f $E5 -l $SPEED
-      beep -f $G5S -l $SPEED
-      beep -f $C6 -l $(($SPEED * 2))
-    '';
-    serviceConfig.Type = "oneshot";
-  };
+  #     beep -f $G5 -l $SPEED
+  #     beep -f $F5S -l $SPEED
+  #     beep -f $D5S -l $SPEED
+  #     beep -f $A4 -l $SPEED
+  #     beep -f $G4S -l $SPEED
+  #     beep -f $E5 -l $SPEED
+  #     beep -f $G5S -l $SPEED
+  #     beep -f $C6 -l $(($SPEED * 2))
+  #   '';
+  #   serviceConfig.Type = "oneshot";
+  # };
 
   users.users.nginx = {
     # for transferring website data
