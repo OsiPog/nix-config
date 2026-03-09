3461f64ce5d470fbdc36eab067b40009a432b4fe
Osi Bluber <87434959+OsiPog@users.noreply.github.com>
2026-03-09 20:42:26 +0100

    fix(llm-tools): disable litellm for now

--- modules/nixos/llm-tools/default.nix
+++ modules/nixos/llm-tools/default.nix
@@ -10,51 +10,51 @@ in {
     secrets."api-keys/anthropic" = {
       sopsFile = ./secrets.yaml;
     };
-    templates.litellm-env = {
-      content = ''
-        ANTHROPIC_API_KEY=${config.sops.placeholder."api-keys/anthropic"}
-      '';
-    };
+    # templates.litellm-env = {
+    #   content = ''
+    #     ANTHROPIC_API_KEY=${config.sops.placeholder."api-keys/anthropic"}
+    #   '';
+    # };
   };
 
   # proxy for gemini cli to use claude models
-  services.litellm = {
-    enable = true;
-    port = 4235;
-    package = pkgs.litellm.overridePythonAttrs (old: rec {
-      version = "7902f3e8040a407e91956e0ed7bb37f000e891d4";
-      src = pkgs.fetchFromGitHub {
-        owner = "WhoisMonesh";
-        repo = "litellm";
-        rev = version;
-        hash = "sha256-w/f+JgQB8QGNuw0jfkQPyH4R3FFyzX/91puEDOv+V0g=";
-      };
-    });
-    environmentFile = config.sops.templates.litellm-env.path;
-    environment = {
-      DETAILED_DEBUG = "True";
-    };
-    settings = {
-      general_settings = {
-        master_key = "sk-1234";
-        allow_requests_on_db_unavailable = true;
-      };
-      litellm_settings = {
-        api_base = "http://localhost:${toString config.services.litellm.port}";
-        drop_params = true;
-      };
-      model_list = [
-        {
-          model_name = claudeModel;
-          litellm_params = {
-            model = "anthropic/${claudeModel}";
-            api_key = "os.environ/ANTHROPIC_API_KEY";
-          };
-        }
-      ];
-      router_settings.model_group_alias = {"gemini-2.5-pro" = claudeModel;};
-    };
-  };
+  # services.litellm = {
+  #   enable = false;
+  #   port = 4235;
+  #   package = pkgs.litellm.overridePythonAttrs (old: rec {
+  #     version = "7902f3e8040a407e91956e0ed7bb37f000e891d4";
+  #     src = pkgs.fetchFromGitHub {
+  #       owner = "WhoisMonesh";
+  #       repo = "litellm";
+  #       rev = version;
+  #       hash = "sha256-w/f+JgQB8QGNuw0jfkQPyH4R3FFyzX/91puEDOv+V0g=";
+  #     };
+  #   });
+  #   environmentFile = config.sops.templates.litellm-env.path;
+  #   environment = {
+  #     DETAILED_DEBUG = "True";
+  #   };
+  #   settings = {
+  #     general_settings = {
+  #       master_key = "sk-1234";
+  #       allow_requests_on_db_unavailable = true;
+  #     };
+  #     litellm_settings = {
+  #       api_base = "http://localhost:${toString config.services.litellm.port}";
+  #       drop_params = true;
+  #     };
+  #     model_list = [
+  #       {
+  #         model_name = claudeModel;
+  #         litellm_params = {
+  #           model = "anthropic/${claudeModel}";
+  #           api_key = "os.environ/ANTHROPIC_API_KEY";
+  #         };
+  #       }
+  #     ];
+  #     router_settings.model_group_alias = {"gemini-2.5-pro" = claudeModel;};
+  #  };
+  # };
 
   home-manager.sharedModules = [
     ({
@@ -78,10 +78,10 @@ in {
           heygptWrapper # terminal gpt integration
         ];
 
-        sessionVariables = {
-          GOOGLE_GEMINI_BASE_URL = nixosConfig.services.litellm.settings.litellm_settings.api_base;
-          GEMINI_API_KEY = nixosConfig.services.litellm.settings.general_settings.master_key;
-        };
+        # sessionVariables = {
+        #   GOOGLE_GEMINI_BASE_URL = nixosConfig.services.litellm.settings.litellm_settings.api_base;
+        #   GEMINI_API_KEY = nixosConfig.services.litellm.settings.general_settings.master_key;
+        # };
       };
 
       sops.secrets = {
@@ -107,19 +107,19 @@ in {
         enable = true;
       };
 
-      programs.opencode = {
-        enable = true;
-        settings = {
-          "$schema" = "https://opencode.ai/config.json";
-          model = "anthropic/${claudeModel}";
-          autoupdate = false;
-          provider.anthropic.options.apiKey = "{file:${config.getSopsFile "api-keys/anthropic"}}";
-          permission = {
-            edit = "ask";
-            bash = "ask";
-          };
-        };
-      };
+      # programs.opencode = {
+      #   enable = true;
+      #   settings = {
+      #     "$schema" = "https://opencode.ai/config.json";
+      #     model = "anthropic/${claudeModel}";
+      #     autoupdate = false;
+      #     provider.anthropic.options.apiKey = "{file:${config.getSopsFile "api-keys/anthropic"}}";
+      #     permission = {
+      #       edit = "ask";
+      #       bash = "ask";
+      #     };
+      #   };
+      # };
     })
   ];
 }
