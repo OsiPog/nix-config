26fe30d3f08bc25d873a077e1e79da055a2ebde0
Osi Bluber <87434959+OsiPog@users.noreply.github.com>
2026-04-17 16:03:35 +0200

    fix(gaming): hytale launcher

--- modules/nixos/gaming.nix
+++ modules/nixos/gaming.nix
@@ -74,7 +74,7 @@
       # Hytale Launcher
       rec {
         appId = "com.hypixel.HytaleLauncher";
-        sha256 = "sha256-ZlsBaIqa0GaTci7QkRnwfcAByS4CL5osXba2p0gE+CQ=";
+        sha256 = "sha256-AeIwVfywttQsGWEOMW7MiOWbZjZpiFWO8AAIe/lVnaM=";
         bundle = toString (pkgs.fetchurl {
           url = "https://launcher.hytale.com/builds/release/linux/amd64/hytale-launcher-latest.flatpak";
           inherit sha256;
