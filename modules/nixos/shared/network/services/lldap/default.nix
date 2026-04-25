ce07834267855e891e1da69c12ff9893d8f78049
Osi Bluber <87434959+OsiPog@users.noreply.github.com>
2026-04-25 17:00:55 +0200

    feat(network/lldap): implement ldap server

--- modules/nixos/shared/network/services/lldap/default.nix
+++ modules/nixos/shared/network/services/lldap/default.nix
@@ -4,9 +4,10 @@
   flake,
   ...
 }: let
-  inherit (builtins) concatStringsSep;
-  inherit (lib) mkIf pipe mkForce;
+  inherit (builtins) concatStringsSep getAttr mapAttrs;
+  inherit (lib) mkIf pipe mkForce mkMerge;
   inherit (lib.strings) splitString;
+  inherit (lib.attrsets) mapAttrs';
 
   inherit (flake.lib) mkNetworkHostServiceModule;
   inherit (config.lib.network) getAddress getServiceVariables;
@@ -17,9 +18,12 @@
     networkCfg
     cfg
     ports
+    stateDir
+    integrationHelpers
     ;
 
   ldapServer = cfg.integrations.ldap.local.server;
+  ldapClients = cfg.integrations.ldap.remote.clients;
 in {
   imports = [
     (mkNetworkHostServiceModule {inherit serviceName;} ({name, ...}: {
@@ -45,19 +49,22 @@ in {
         adminUser = {
           dn = "admin";
           secret = {
-            key = "lldap/admin-pass";
-            group = "lldap-admin-pass";
-            mode = "0440";
             sopsFile = ./secrets.yaml;
+            key = "lldap/admin-pass";
           };
         };
         searchUser = {
-          dn = "search";
+          dn = "search-user";
           secret = {
-            key = "lldap/search-pass";
             sopsFile = ./secrets.yaml;
-            group = "ldap-search";
-            mode = "0440";
+            key = "lldap/search-pass";
+          };
+        };
+        managerUser = {
+          dn = "manager";
+          secret = {
+            sopsFile = ./secrets.yaml;
+            key = "lldap/manager-pass";
           };
         };
       };
@@ -67,80 +74,155 @@ in {
     flake.nixosModules.lldapBootstrap
   ];
 
-  config = mkIf (networkCfg.enable && cfg.enable) {
-    assertions = [
-      {
-        assertion = ports.ldaps.reverseProxy.enable;
-        message = "ldaps port needs to be reverse proxied to ensure the server can be reached on a domain.";
-      }
-      {
-        assertion = ports.lldap.reverseProxy.enable;
-        message = "lldap port needs to be reverse proxied to ensure the server can be reached on a domain.";
-      }
-    ];
-
-    sops.secrets = {
-      ${ldapServer.adminUser.secret.key} =
-        ldapServer.adminUser.secret
-        // {
-          user = "lldap";
-        };
-    };
-
-    users.groups.${ldapServer.adminUser.group} = {};
-
-    # TLS
-    services.porkbunAcme = {
-      enable = true;
-      domain = ldapServer.address "domain";
-    };
-
-    users.groups.lldap = {};
-    users.users.lldap = {
-      group = "lldap";
-      isSystemUser = true;
-
-      # Give lldap user access to ACME certificates
-      extraGroups = ["acme"];
-    };
-
-    # LLDAP service configuration
-    services.lldap = {
-      enable = true;
-      settings = {
-        verbose = true;
-        http_host = "0.0.0.0";
-        http_port = ports.lldap.port;
-        ldap_base_dn = cfg.ldap.baseDN;
-        ldap_user_dn = cfg.ldap.userDN;
-        ldap_user_pass_file = config.getSopsFile "lldap/admin-pass";
-        database_url = "sqlite://${cfg.stateDir}/users.db?mode=rwc";
-        force_ldap_user_pass_reset = "always";
-        ldaps_options = let
-          acmeDirectory = config.security.acme.certs.${ldapServer.address "domain"}.directory;
-        in {
-          enabled = true;
-          port = ports.ldaps.port;
-          cert_file = "${acmeDirectory}/cert.pem";
-          key_file = "${acmeDirectory}/key.pem";
-        };
+  config = mkIf (networkCfg.enable && cfg.enable) (mkMerge [
+    # register secrets with read access for the lldap user
+    (integrationHelpers.ldap.mkRegisterIntegrationSecretsConfig {
+      secrets = {
+        adminUserPass = ldapServer.adminUser.secret;
+        searchUserPass = ldapServer.searchUser.secret;
+        managerUserPass = ldapServer.managerUser.secret;
       };
-      bootstrap = {
-        enable = true;
-        cleanup = {
-          enable = true;
-          keepUsers = true;
-          keepGroupMembership = true;
-        };
-      };
-    };
+      users = ["lldap"];
+    })
 
-    systemd = {
-      services.lldap.serviceConfig.DynamicUser = mkForce false;
-      # Ensure state directory is owned by lldap
-      tmpfiles.rules = [
-        "d ${cfg.stateDir} 0750 lldap lldap -"
+    {
+      assertions = [
+        {
+          assertion = ports.ldaps.reverseProxy.enable;
+          message = "ldaps port needs to be reverse proxied to ensure the server can be reached on a domain.";
+        }
+        {
+          assertion = ports.lldap.reverseProxy.enable;
+          message = "lldap port needs to be reverse proxied to ensure the server can be reached on a domain.";
+        }
       ];
-    };
-  };
+
+      # TLS
+      services.porkbunAcme = {
+        enable = true;
+        domain = ldapServer.address "domain";
+      };
+
+      users.groups.lldap = {};
+      users.users.lldap = {
+        group = "lldap";
+        isSystemUser = true;
+
+        # Give lldap user access to ACME certificates
+        extraGroups = ["acme"];
+      };
+
+      # LLDAP service configuration
+      services.lldap = {
+        enable = true;
+        settings = {
+          verbose = true;
+          http_host = "0.0.0.0";
+          http_port = ports.lldap.port;
+          ldap_base_dn = ldapServer.baseDN;
+          ldap_user_dn = ldapServer.adminUser.dn;
+          ldap_user_pass_file = integrationHelpers.ldap.getSopsFile "adminUserPass";
+          database_url = "sqlite://${stateDir}/users.db?mode=rwc";
+          force_ldap_user_pass_reset = "always";
+          ldaps_options = let
+            acmeDirectory = config.security.acme.certs.${ldapServer.address "domain"}.directory;
+          in {
+            enabled = true;
+            port = ports.ldaps.port;
+            cert_file = "${acmeDirectory}/cert.pem";
+            key_file = "${acmeDirectory}/key.pem";
+          };
+        };
+        bootstrap = {
+          enable = true;
+          users.configs = {
+            ${ldapServer.searchUser.dn} = {
+              email = "search-user@example.com";
+              password_file = integrationHelpers.ldap.getSopsFile "searchUserPass";
+              groups = [
+                "lldap_strict_readonly"
+              ];
+            };
+            ${ldapServer.managerUser.dn} = {
+              email = "manager-user@example.com";
+              password_file = integrationHelpers.ldap.getSopsFile "managerUserPass";
+              groups = [
+                "lldap_password_manager" # has rw permissions on users
+              ];
+            };
+          };
+          cleanup = {
+            enable = true;
+            keepUsers = true;
+            keepGroupMembership = true;
+          };
+        };
+      };
+
+      systemd = {
+        services.lldap.serviceConfig.DynamicUser = mkForce false;
+        # Ensure state directory is owned by lldap
+        tmpfiles.rules = [
+          "d ${stateDir} 0750 lldap lldap -"
+        ];
+      };
+    }
+
+    # LDAP INTEGRATION
+    (mkIf (cfg.integrations.ldap.enable) (pipe ldapClients [
+      (map ({
+        createGroups,
+        createUsers,
+        createUserAttributes,
+      }:
+        (
+          integrationHelpers.ldap.mkRegisterIntegrationSecretsConfig {
+            secrets =
+              mapAttrs' (name: {secret, ...}: {
+                name = "${name}UserPass";
+                value = secret;
+              })
+              createUsers;
+            users = ["lldap"];
+          }
+        )
+        // {
+          services.lldap.bootstrap = {
+            groups.configs = createGroups;
+            users = {
+              schemas =
+                mapAttrs (_: {
+                  dataType,
+                  editable,
+                  multiple,
+                  visible,
+                }: {
+                  attributeType = getAttr dataType {
+                    string = "STRING";
+                    integer = "INTEGER";
+                    boolean = throw "Type boolean is not implemented in LLDAP.";
+                    jpeg = "JPEG";
+                    datetime = "DATETIME";
+                  };
+                  isEditable = editable;
+                  isList = multiple;
+                  isVisible = visible;
+                })
+                createUserAttributes;
+              configs =
+                mapAttrs (name: {
+                  display,
+                  email,
+                  ...
+                }: {
+                  inherit email;
+                  displayName = display;
+                  password_file = integrationHelpers.ldap.getSopsFile "${name}UserPass";
+                })
+                createUsers;
+            };
+          };
+        }))
+    ]))
+  ]);
 }
