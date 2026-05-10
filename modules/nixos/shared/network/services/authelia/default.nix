890d7e046afadf4e807a865f2de2492dc634ba97
Osi Bluber <87434959+OsiPog@users.noreply.github.com>
2026-05-10 22:15:39 +0200

    refactor: opencloud, authelia, jellyfin rewrite require ldap (untested)

--- modules/nixos/shared/network/services/authelia/default.nix
+++ modules/nixos/shared/network/services/authelia/default.nix
@@ -5,9 +5,10 @@
   hostName,
   ...
 }: let
-  inherit (lib) mkIf mkOption;
+  inherit (lib) mkIf mkMerge mkDefault;
+  inherit (lib.attrsets) getAttrs;
 
-  inherit (flake.lib) mkNetworkHostServiceModule;
+  inherit (flake.lib) mkNetworkHostServiceModule mkGroupsFromSecretsWithMembers;
   inherit (config.lib.network) getAddress getServiceVariables;
 
   inherit
@@ -18,70 +19,90 @@
     cfg
     ports
     ;
+
+  stateDir = "/var/lib/authelia-default";
+  address = getAddress {
+    inherit hostName;
+    portName = "authelia";
+  };
 in {
   imports = [
     (mkNetworkHostServiceModule {inherit serviceName;} ({...}: {
-      optionsService.mailHost = mkOption {
-        description = "The host on which the mailserver is running";
-        type = with lib.types; nullOr str;
-        default = null;
+      configEnable.ports.${portName} = {
+        protocol = "https";
+        port = 9091;
       };
-      configEnable = {
-        ports.${portName}.port = 9091;
-      };
-      configService.stateDir = "/var/lib/authelia-default"; # hardcoded in nixpkgs
     }))
   ];
 
-  config = mkIf (networkCfg.enable && cfg.enable) {
-    assertions = [
-      {
-        assertion = ports.authelia.reverseProxy.enable;
-        message = "Authelia needs to be reverse proxied as https is required.";
-      }
-    ];
+  config = mkIf (networkCfg.enable && cfg.enable) (mkMerge [
+    {
+      assertions = [
+        {
+          assertion = ports.authelia.reverseProxy.enable;
+          message = "Authelia needs to be reverse proxied as https is required.";
+        }
+      ];
 
-    # Authelia secrets
-    sops.secrets = {
-      "authelia/jwtSecret" = {
-        owner = config.services.authelia.instances.default.user;
-        sopsFile = ./secrets.yaml;
+      # Authelia secrets
+      sops.secrets = {
+        "authelia/jwtSecret" = {
+          owner = config.services.authelia.instances.default.user;
+          sopsFile = ./secrets.yaml;
+        };
+        "authelia/storageEncryptionKey" = {
+          owner = config.services.authelia.instances.default.user;
+          sopsFile = ./secrets.yaml;
+        };
+        "authelia/oidcIssuerPrivateKeyFile" = {
+          owner = config.services.authelia.instances.default.user;
+          sopsFile = ./secrets.yaml;
+        };
       };
-      "authelia/storageEncryptionKey" = {
-        owner = config.services.authelia.instances.default.user;
-        sopsFile = ./secrets.yaml;
-      };
-      "authelia/oidcIssuerPrivateKeyFile" = {
-        owner = config.services.authelia.instances.default.user;
-        sopsFile = ./secrets.yaml;
-      };
-    };
 
-    # Authelia service configuration
-    services.authelia.instances.default = {
-      enable = true;
-      secrets = {
-        jwtSecretFile = config.getSopsFile "authelia/jwtSecret";
-        storageEncryptionKeyFile = config.getSopsFile "authelia/storageEncryptionKey";
-        oidcIssuerPrivateKeyFile = config.getSopsFile "authelia/oidcIssuerPrivateKeyFile";
+      # Authelia service configuration
+      services.authelia.instances.default = {
+        enable = true;
+        secrets = {
+          jwtSecretFile = config.getSopsFile "authelia/jwtSecret";
+          storageEncryptionKeyFile = config.getSopsFile "authelia/storageEncryptionKey";
+          oidcIssuerPrivateKeyFile = config.getSopsFile "authelia/oidcIssuerPrivateKeyFile";
+        };
+        settings = {
+          server.address = "tcp://:${toString ports.${portName}.port}";
+          log.level = "info";
+          storage.local.path = "${stateDir}/db.sqlite3";
+          session.cookies = [
+            {
+              domain = address "domain";
+              authelia_url = address "protocol://domain";
+            }
+          ];
+          access_control.default_policy = "two_factor";
+        };
       };
-      settings = {
-        server.address = "tcp://:${toString ports.${portName}.port}";
-        log.level = "info";
-        storage.local.path = "${cfg.stateDir}/db.sqlite3";
-        session.cookies = [
-          {
-            domain = getAddress {
-              portName = "authelia";
+    }
+
+    # LDAP INTEGRATION
+    (let
+      ldapServer = cfg.require.ldap-server;
+      secrets = getAttrs [ldapServer.users.manage.secretName] ldapServer.secrets;
+    in
+      mkIf (ldapServer != null) {
+        sops = {inherit secrets;};
+        users.groups = mkGroupsFromSecretsWithMembers secrets [config.services.authelia.instances.default.user];
+        services.authelia.instances.default = {
+          environmentVariables.AUTHELIA_AUTHENTICATION_BACKEND_LDAP_PASSWORD_FILE = config.getSopsFile ldapServer.users.manage.secretName;
+          settings.authentication_backend = {
+            refresh_interval = mkDefault "1m";
+            ldap = {
+              implementation = "lldap";
+              address = ldapServer.address "protocol://domain:port";
+              base_dn = ldapServer.baseDN;
+              user = "uid=${ldapServer.users.manage.dn},ou=people,${ldapServer.baseDN}";
             };
-            authelia_url = getAddress {
-              protocol = "https";
-              portName = "authelia";
-            };
-          }
-        ];
-        access_control.default_policy = "two_factor";
-      };
-    };
-  };
+          };
+        };
+      })
+  ]);
 }
--- modules/nixos/shared/network/services/opencloud/default.nix
+++ modules/nixos/shared/network/services/opencloud/default.nix
@@ -5,8 +5,9 @@
   hostName,
   ...
 }: let
-  inherit (lib) mkIf mkDefault;
-  inherit (flake.lib) mkNetworkHostServiceModule;
+  inherit (lib) mkIf mkMerge mkDefault;
+  inherit (lib.attrsets) filterAttrs;
+  inherit (flake.lib) mkNetworkHostServiceModule mkGroupsFromSecretsWithMembers;
   inherit (config.lib.network) getServiceVariables getAddress;
 
   inherit
@@ -16,31 +17,76 @@
     cfg
     ports
     portName
+    stateDir
     ;
+  address = getAddress {
+    inherit portName;
+    inherit hostName;
+  };
 in {
   imports = [
     (mkNetworkHostServiceModule {inherit serviceName;} ({...}: {
-      configEnable.ports.${portName}.port = mkDefault 9200;
+      configEnable.ports.${portName} = {
+        protocol = "https";
+        port = mkDefault 9200;
+      };
+      provideEnable.ldap-clients = [{groups.cloud = {};}];
     }))
   ];
 
-  config = mkIf (networkCfg.enable && cfg.enable) {
-    services.opencloud = {
-      enable = true;
-      stateDir = cfg.stateDir;
-      address = "0.0.0.0";
-      port = ports.opencloud.port;
-      url = getAddress {
-        protocol = "https";
-        inherit portName;
-        inherit hostName;
+  config = mkIf (networkCfg.enable && cfg.enable) (mkMerge [
+    {
+      services.opencloud = {
+        inherit stateDir;
+        enable = true;
+        address = "0.0.0.0";
+        port = ports.opencloud.port;
+        url = address "protocol://domain";
+        environment = {
+          PROXY_TLS = "false"; # TLS handled by reverse proxy
+        };
+        # settings = {
+        #   proxy.proxy.http.tls = false;
+        # };
       };
-      environment = {
-        PROXY_TLS = "false"; # TLS handled by reverse proxy
-      };
-      # settings = {
-      #   proxy.proxy.http.tls = false;
-      # };
-    };
-  };
+    }
+
+    # LDAP INTEGRATION
+    (let
+      ldapServer = cfg.require.ldap-server;
+      secrets = filterAttrs (name: _: name == ldapServer.users.search.secretName) ldapServer.secrets;
+    in
+      mkIf (ldapServer != null) {
+        sops = {
+          inherit secrets;
+          templates.opencloud-env = {
+            content = ''
+              OC_LDAP_BIND_PASSWORD=${config.sops.placeholder.${ldapServer.users.search.secretName}}
+            '';
+            owner = "opencloud";
+          };
+        };
+        users.groups = mkGroupsFromSecretsWithMembers secrets [config.services.opencloud.user];
+        services.opencloud.environment = {
+          OC_LDAP_URI = ldapServer.address "protocol://domain:port";
+          OC_LDAP_BIND_DN = "uid=${ldapServer.users.search.dn},ou=people,${ldapServer.baseDN}";
+          OC_ADMIN_USER_ID = ldapServer.users.admin.dn;
+
+          OC_LDAP_USER_BASE_DN = "ou=people,${ldapServer.baseDN}";
+          OC_LDAP_USER_FILTER = "(|(memberof=cn=cloud,ou=groups,${ldapServer.baseDN})(uid=${ldapServer.users.admin.dn}))";
+          OC_LDAP_USER_ENABLED_ATTRIBUTE = "uid";
+          OC_LDAP_USER_SCHEMA_ID = "uid";
+          OC_LDAP_USER_SCHEMA_USER_TYPE = "";
+          OC_LDAP_CACERT = config.security.pki.caBundle;
+
+          OC_LDAP_GROUP_BASE_DN = "ou=groups,${ldapServer.baseDN}";
+          OC_LDAP_GROUP_SCHEMA_ID = "uid";
+          OC_LDAP_GROUP_SCHEMA_MAIL = "";
+
+          OC_LDAP_SERVER_WRITE_ENABLED = "false";
+        };
+
+        systemd.services.opencloud.serviceConfig.EnvironmentFile = [config.sops.templates.opencloud-env.path];
+      })
+  ]);
 }
